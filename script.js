0114881c1749bff8d79dccddeff7fe0c7d82381d
raaj648 <raajkhan000648@gmail.com>
2025-10-04 14:25:39 +0600

    Update script.js

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ function loadTopCategories() {
   const leftBtn = section.querySelector("#cat-left");
   const rightBtn = section.querySelector("#cat-right");
   leftBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"><path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/></svg>`;
-  rightBtn.innerHTML = `<svg xmlns="http://www.w.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg>`;
+  rightBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg>`;
   setupCarouselPagination(container, leftBtn, rightBtn);
 }
 
@@ -288,4 +288,5 @@ document.addEventListener("DOMContentLoaded", async () => {
   await searchDataPromise;
   setupSearch();
 });
+
 
